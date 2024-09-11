0ead4fb1e0dd79fe8a5130dd7df0d49fb276cca3
jhson <aqua178@naver.com>
2024-09-11 10:58:01 +0900

    포미션

--- work/2408_permission_guide/js/ui.js
+++ work/2408_permission_guide/js/ui.js
@@ -1,6 +1,6 @@
 /*
 File  : ui.js
-Date  : 2024.09.05
+Date  : 2024.09.11
 menu  : 공통 js
 */
 
@@ -25,9 +25,37 @@ var clickOff = function (){
 
 
 
+//* bubbleBox */
+var bubbleBox = function() {
+  var $footer = $('footer'); 
+  var $btnClose = $('.bubbleBox button.close');
+
+  // footer 클릭 시 display: block인 bubbleBox를 숨깁니다.
+  $footer.on('click', function(event) {
+    // footer 내의 모든 .bubbleBox 검사
+    $footer.find('.bubbleBox').each(function() {
+       var display = $(this).css('display'); // 요소의 display 속성 값을 가져옵니다.
+       if (display === 'block' || display === 'flex') {
+        $(this).css('display', 'none');
+      }
+    });
+    // 이벤트 전파를 막아, 문서의 다른 클릭 이벤트와 충돌하지 않도록 합니다.
+    event.stopPropagation();
+  });
+
+  // 닫기 버튼 클릭 시 해당 bubbleBox 요소를 숨깁니다.
+  $btnClose.on('click', function(event) {
+    $(this).closest('.bubbleBox').css('display', 'none');
+    // 이벤트 전파를 막아, 문서의 다른 클릭 이벤트와 충돌하지 않도록 합니다.
+    event.stopPropagation();
+  });
+};
+
+
 //* common */
 $(function () {
   modalOpen();
   clickOff();
+  bubbleBox();
 });
 
