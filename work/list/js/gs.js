2e7474e52c09fadfb9172b6489e220491f8e294d
jhson <aqua178@naver.com>
2025-02-14 11:21:31 +0900

    포미션리스트

---
+++ work/list/js/gs.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', ()=>{
+
+    var Wrap = document.querySelector(".wrap");
+
+    Wrap.insertAdjacentHTML('afterbegin', '<div class="header"><span class="btn_left"></span><span class="title">우리동네 놀이터</span><span class="btn_right">문의<br/>하기</span></div>');
+
+});
